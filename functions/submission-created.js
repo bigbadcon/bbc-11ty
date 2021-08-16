855c979ee8ea2f3310b50452729328c5f448917a
Colin Fahrion <colin.fahrion@gmail.com>
2021-08-15 21:00:05 -0700

    refactor; fixed issue where function returned early. Also refactored send grid to use await

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -34,95 +34,92 @@ exports.handler = async function(event, context) {
             // twitter: twitter
         }
 
+        /* -------------------------------------------------------------------------- */
+        /*                          Attempt to create account                         */
+        /* -------------------------------------------------------------------------- */
         try {
             const res = await axios.put(apiBaseUrl + 'users/create', params)
             // console.log("put response", res);
             console.log("New user successfully created for", userNicename, userEmail, displayName);
 
-            /* --------------------------- New user message ---------------------------- */
-            const newUserMsg = {
-                to: userEmail,
-                from: 'info@bigbadcon.com', 
-                subject: 'Big Bad Con New User Account',
-                text: `Welcome ${displayName}! Your new user account has been created. You can now return to bigbadcon.com to log in!`,
-                html: `Welcome ${displayName}! Your new user account has been created. You can now return to <a href="http://www.bigbadcon.com>Big Bad Con</a> to log in!`,
-            }
-            /* ---------------------------- SendGrid function --------------------------- */
-            sgMail
-            .send(newUserMsg)
-            .then(() => {
-                console.log(`New User Success Email sent to ${userEmail}`)
-            })
-            .catch((error) => {
-                console.error(error)
-            })
+            /* -------------------------------------------------------------------------- */
+            /*                     If successful try above send emails                    */
+            /* -------------------------------------------------------------------------- */
+            try {
+                /* --------------------------- New user message ---------------------------- */
+                const newUserMsg = {
+                    to: userEmail,
+                    from: 'info@bigbadcon.com', 
+                    subject: 'Big Bad Con New User Account',
+                    text: `Welcome ${displayName}! Your new user account has been created. You can now return to bigbadcon.com to log in!`,
+                    html: `Welcome ${displayName}! Your new user account has been created. You can now return to <a href="http://www.bigbadcon.com>Big Bad Con</a> to log in!`,
+                }
 
-            /* --------------------------- Admin user message --------------------------- */
-            const newUserAdminMsg = {
-                to: 'info@bigbadcon.com',
-                from: 'info@bigbadcon.com', 
-                subject: 'New User added',
-                text: `New user ${displayName} added! Email: ${userEmail}; Full name: ${firstName} ${lastName}; userNicename: ${userNicename}`,
-                html: `New user ${displayName} added! Email: ${userEmail}; Full name: ${firstName} ${lastName}; userNicename: ${userNicename}`,
-            }
-            /* ---------------------------- SendGrid function --------------------------- */
-            sgMail
-            .send(newUserAdminMsg)
-            .then(() => {
-                console.log(`Admin Email sent for ${userEmail}`)
-            })
-            .catch((error) => {
-                console.error(error)
-            })
+                await sgMail.send(newUserMsg);
+                /* --------------------------- Admin user message --------------------------- */
+                const newUserAdminMsg = {
+                    to: 'info@bigbadcon.com',
+                    from: 'info@bigbadcon.com', 
+                    subject: 'New User added',
+                    text: `New user ${displayName} added! Email: ${userEmail}; Full name: ${firstName} ${lastName}; userNicename: ${userNicename}`,
+                    html: `New user ${displayName} added! Email: ${userEmail}; Full name: ${firstName} ${lastName}; userNicename: ${userNicename}`,
+                }
+                await sgMail.send(newUserAdminMsg);
 
-            /* ------------------------------- Return 200 ------------------------------- */
-            return {
-                statusCode: 200,
-                body: "account submitted"
+                // finalize function
+                return {
+                    statusCode: 200,
+                    body: "account submitted and emails sent"
+                }
+            } catch (e) {
+                return {
+                    statusCode: e.response.status,
+                    body: "Account error with sending emails"
+                }
             }
+
         } catch(e) {
+            /* -------------------------------------------------------------------------- */
+            /*                      Catch for failed Account creation                     */
+            /* -------------------------------------------------------------------------- */
             console.log("account submission error", e);
 
-            /* --------------------------- New user message ---------------------------- */
-            const newUserMsg = {
-                to: userEmail,
-                from: 'info@bigbadcon.com', 
-                subject: 'Big Bad Con New User Account',
-                text: `Hello ${displayName}, Unfortunately there was a problem adding your account. This could have been for several reasons including if there already was a user by that name or email in the system. Or just server error. An email has been sent to our admin staff to see what is wrong. If you have any questions you can reply to this message.`,
-                html: `Hello ${displayName}, Unfortunately there was a problem adding your account. This could have been for several reasons including if there already was a user by that name or email in the system. Or just server error. An email has been sent to our admin staff to see what is wrong. If you have any questions you can reply to this message.`,
-            }
-            /* ---------------------------- SendGrid function --------------------------- */
-            sgMail
-            .send(newUserMsg)
-            .then(() => {
-                console.log(`Failed New User Email sent to ${userEmail}`)
-            })
-            .catch((error) => {
-                console.error(error)
-            })
+            /* -------------------------------------------------------------------------- */
+            /*                    Seend emails for failed account creation                */
+            /* -------------------------------------------------------------------------- */
+            try {
+                /* --------------------------- New user message ---------------------------- */
+                const newUserMsg = {
+                    to: userEmail,
+                    from: 'info@bigbadcon.com', 
+                    subject: 'Big Bad Con New User Account',
+                    text: `Hello ${displayName}, Unfortunately there was a problem adding your account. This could have been for several reasons including if there already was a user by that name or email in the system. Or just server error. An email has been sent to our admin staff to see what is wrong. If you have any questions you can reply to this message.`,
+                    html: `Hello ${displayName}, Unfortunately there was a problem adding your account. This could have been for several reasons including if there already was a user by that name or email in the system. Or just server error. An email has been sent to our admin staff to see what is wrong. If you have any questions you can reply to this message.`,
+                }
 
-            /* --------------------------- Admin user message --------------------------- */
-            const newUserAdminMsg = {
-                to: 'info@bigbadcon.com',
-                from: 'info@bigbadcon.com', 
-                subject: 'New User Account Creation Failed',
-                text: `The user ${displayName} attempted but failed to create an account. Not sure why it failed. Email: ${userEmail}; Full name: ${firstName} ${lastName}; userNicename: ${userNicename}`,
-                html: `The user ${displayName} attempted but failed to create an account. Not sure why it failed. Email: ${userEmail}; Full name: ${firstName} ${lastName}; userNicename: ${userNicename}`,
-            }
-            /* ---------------------------- SendGrid function --------------------------- */
-            sgMail
-            .send(newUserAdminMsg)
-            .then(() => {
-                console.log(`Failed New User Email sent to admin about ${userEmail}`)
-            })
-            .catch((error) => {
-                console.error(error)
-            })
+                await sgMail.send(newUserMsg);
+                /* --------------------------- Admin user message --------------------------- */
+                const newUserAdminMsg = {
+                    to: 'info@bigbadcon.com',
+                    from: 'info@bigbadcon.com', 
+                    subject: 'New User Account Creation Failed',
+                    text: `The user ${displayName} attempted but failed to create an account. Not sure why it failed. Email: ${userEmail}; Full name: ${firstName} ${lastName}; userNicename: ${userNicename}`,
+                    html: `The user ${displayName} attempted but failed to create an account. Not sure why it failed. Email: ${userEmail}; Full name: ${firstName} ${lastName}; userNicename: ${userNicename}`,
+                }
+                await sgMail.send(newUserAdminMsg);
 
-            return {
-                statusCode: e.response.status,
-                body: "error in account submission"
+                // finalize function
+                return {
+                    statusCode: 500,
+                    body: "account creation failed and emails sent"
+                }
+            } catch (e) {
+                return {
+                    statusCode: e.response.status,
+                    body: "account creation failed. now emails sent"
+                }
             }
+            
         }
  
     }
