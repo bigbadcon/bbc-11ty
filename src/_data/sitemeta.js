07eb508777b118c250a33804198c974f0f88a406
Colin Fahrion <colin.fahrion@gmail.com>
2021-08-17 13:41:00 -0700

    fix: typo

--- src/_data/sitemeta.js
+++ src/_data/sitemeta.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 module.exports = {
     domain: process.env.DOMAIN || "www.bigbadcon.com",
     url: process.env.URL || "https://www.bigbadcon.com",
-    eventDate: "Oct 23nd-24th 2021",
+    eventDate: "Oct 23rd-24th 2021",
 };
