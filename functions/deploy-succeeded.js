c841d111ff299e875c57968ac6f4d7eb45d66a7c
Colin Fahrion <colin.fahrion@gmail.com>
2022-04-02 20:30:00 +0000

    feat: added Slack webhook to deploy-succeeded function

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -1,3 +1,5 @@
+const querystring = require("querystring");
+const fetch = require("node-fetch");
 
 // const { URL } = process.env
 // URL === your site URL
@@ -5,17 +7,41 @@
 exports.handler = async (event, context) => {
   
   console.log('deploy-succeeded')
-  console.log('event', event)
-  console.log('context', context)
+  
   if (event.body) {
-    let body = JSON.parse(event.body);
-    console.log('payload',body.payload);
+    const payload = JSON.parse(event.body).payload
+    
+    // Only allow POST
+    if (event.httpMethod !== "POST") {
+      return { statusCode: 405, body: "Method Not Allowed" };
+    }
+    
+    // When the method is POST, the name will no longer be in the event’s
+    // queryStringParameters – it’ll be in the event body encoded as a queryString
+    
+    // Send message with link to Slack
+    return fetch(process.env.SLACK_WEBHOOK_URL, {
+      headers: {
+        "content-type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({ text: `${payload.title} build has successfully depoloyed. Preview permalink: ${payload.links.permalink}` }),
+    })
+      .then(() => ({
+        statusCode: 200,
+        body: `The deploy message for ${payload.title} has been sent to Slack 👋`,
+      }))
+      .catch((error) => ({
+        statusCode: 422,
+        body: `Oops! Something went wrong. ${error}`,
+      }));
+      
+  } else {
+    return {
+      statusCode: 422,
+      body: `missing event.body`,
+    }
   }
   
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      data: 'yay'
-    })
-  }
+  
 }
