966ef81705f280bd9a5fe2b9c8a6f5b174f540d5
Colin Fahrion <colin.fahrion@gmail.com>
2022-04-02 18:08:13 +0000

    test: testing deploy succeeded function

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -2,14 +2,21 @@ const fetch = require('node-fetch')
 const { URL } = process.env
 // URL === your site URL
 
-const functionOne = `${URL}/.netlify/functions/check-registration/493/colinaut/`
-const functionTwo = `${URL}/.netlify/functions/screenshot/online-interactive-fiction-collaborative-storytelling-games/`
+// const functionOne = `${URL}/.netlify/functions/check-registration/493/colinaut/`
+// const functionTwo = `${URL}/.netlify/functions/screenshot/online-interactive-fiction-collaborative-storytelling-games/`
 
 exports.handler = async (event, context) => {
+  
+  console.log('deploy-succeeded')
+  console.log('event', event)
+  console.log('context', context)
 
-  // Modify fetch calls with your payloads (if needed)
-  const pingOne = await fetch(functionOne)
-  const pingTwo = await fetch(functionTwo)
+  // Ping serverless functions to renew cache
+  // TODO: do I still need this?
+  // const pingOne = await fetch(functionOne)
+  // const pingTwo = await fetch(functionTwo)
+  
+  
   
   return {
     statusCode: 200,
