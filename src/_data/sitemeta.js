acff55b9332b23a926cea38f72d458ab2f228d9a
Colin Fahrion <colin.fahrion@gmail.com>
2022-04-04 02:50:55 +0000

    content: start update for Big Bad Online 2022

--- src/_data/sitemeta.js
+++ src/_data/sitemeta.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 module.exports = {
     domain: process.env.DOMAIN || "www.bigbadcon.com",
     url: process.env.URL || "https://www.bigbadcon.com",
-    eventDate: "Returning in 2022",
+    eventDate: "Online May 6-7, 2022",
 };
