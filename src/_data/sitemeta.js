08781a3f8fbb5c8b46baa2120c5eb74c74acfa6a
Colin Fahrion <colin.fahrion@gmail.com>
2022-05-31 10:35:26 -0700

    changes

--- src/_data/sitemeta.js
+++ src/_data/sitemeta.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 module.exports = {
     domain: process.env.DOMAIN || "www.bigbadcon.com",
     url: process.env.URL || "https://www.bigbadcon.com",
-    eventDate: "Online May 6-7, 2022",
+    eventDate: "Oct 27-30, 2022",
 };
