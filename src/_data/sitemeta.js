f6b53423240d988b00ce6c5acbc2c8b53a6b6a3e
Colin Fahrion <colin.fahrion@gmail.com>
2022-08-29 13:04:10 -0700

    feat: validation log only in dev

--- src/_data/sitemeta.js
+++ src/_data/sitemeta.js
@@ -3,5 +3,6 @@ require('dotenv').config();
 module.exports = {
     domain: process.env.DOMAIN || "www.bigbadcon.com",
     url: process.env.URL || "https://www.bigbadcon.com",
+    environment: process.env.ELEVENTY_ENV,
     eventDate: "Oct 27-30, 2022",
 };
