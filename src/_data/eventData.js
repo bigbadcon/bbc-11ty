c739810669b6328b3e2d672d7c7dc1d7142d5fec
Colin Fahrion <colin.fahrion@gmail.com>
2022-08-31 16:51:44 -0700

    fix: rely on eventId if no eventSlug

--- src/_data/eventData.js
+++ src/_data/eventData.js
@@ -1,6 +1,7 @@
 const Cache = require("@11ty/eleventy-cache-assets");
 const windows1252 = require('windows-1252');
 const utf8 = require('utf8')
+const { v4: uuidv4 } = require('uuid');
 const slugify = require('slugify')
 // const rootCas = require('ssl-root-cas').create();
 var dayjs = require('dayjs')
@@ -46,16 +47,18 @@ module.exports = async () => {
         // TODO: issue found is that eventStatus = null when it is moved to trash back to drafts?
         data = data.filter(event => event.eventStatus === 1);
         // TODO: fix this as it is a kludge as we are hard coding the date
-        // data = data.filter(event => dayjs(event.eventStartTime).isAfter(dayjs('2022-09-01')));
+        data = data.filter(event => dayjs(event.eventStartDate).isAfter(dayjs('2022-09-01')));
 
         /* ---------------- fix data if missing slug and decode text ---------------- */
         data = data.map(event => {
-            if (!event.eventSlug || event.eventSlug === "") event = {...event, eventSlug: slugify(event.eventName,{strict: true})}
+            // if it is missing the eventSlug then use the eventId
+            if (!event.eventSlug) event.eventSlug = event.eventId.toString()
+
             // Convert metadata array to a more usable keyed object
             let metadata = metadataArrayToObject(event.metadata)
             // decode GM field text to proper format
             // TODO: refactor so that both this and the past events works the same? Right now we convert this to an object here but with past events we use a filter. Might be nice if we did it on the server API side though.
-            metadata = {...metadata, GM: (metadata.GM) && decodeText(metadata.GM) }
+            metadata = {...metadata, GM: (metadata.GM) && decodeText(metadata.GM), System: (metadata.System) && decodeText(metadata.System) }
 
             // Create Javascript date objects
             const eventStartDateTime = dayjs(event.eventStartDate + "T" + event.eventStartTime + "-07:00").toDate()
@@ -66,7 +69,7 @@ module.exports = async () => {
             return {
                 ...event,
                 eventName: decodeText(event.eventName), // change to proper format
-                postContent: decodeText(event.postContent), // change to proper format
+                postContent: decodeText(event.postContent),// change to proper format
                 metadata: metadata, // replace with keyed object
                 eventStartDateTime: eventStartDateTime, // native javascript date object
                 eventEndDateTime: eventEndDateTime, // native javascript date object
@@ -97,7 +100,7 @@ module.exports = async () => {
         }
 
     } catch(error) {
-        console.log(error)
+        console.log('error',error)
         return {
             events: [],
             volunteer: [],
