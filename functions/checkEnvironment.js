428e88e562ab43034dad8cb6ae0331f9123673af
Colin Fahrion <colin.fahrion@gmail.com>
2022-08-31 20:37:22 -0700

    feat: update to checkEnv

--- functions/checkEnvironment.js
+++ functions/checkEnvironment.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
 exports.handler = async function(event, context) {
-    console.log(process.env)
     const buildContext = process.env.CONTEXT
     const ELEVENTY_ENV = process.env.ELEVENTY_ENV
-    console.log
     return {
         statusCode: 200,
         body: `CONTEXT: ${buildContext} ELEVENTY_ENV: ${ELEVENTY_ENV}`
--- src/_data/sitemeta.js
+++ src/_data/sitemeta.js
@@ -4,5 +4,6 @@ module.exports = {
     domain: process.env.DOMAIN || "www.bigbadcon.com",
     url: process.env.URL || "https://www.bigbadcon.com",
     environment: process.env.ELEVENTY_ENV,
+    context: process.env.CONTEXT,
     eventDate: "Oct 27-30, 2022",
 };
