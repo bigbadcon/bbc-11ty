edd71e1ba839a8adf5ae6f7deb268bb492a89f87
Jeremy Tidwell <necanthrope@users.noreply.github.com>
2022-09-02 12:35:07 -0700

    Front end support for Eventbrite payment processor

--- src/static/js/eventbrite-payment.js
+++ src/static/js/eventbrite-payment.js
@@ -37,9 +37,12 @@ const eventbriteOrderCallback = async function () {
         method: 'POST',
         body: {orderId: orderCode}
     })
-    console.log("Eventbrite order code submit response", res)
-    if (res.status === 200) { //&& res.headers.get('authorization')) {
-        console.log("Eventbrite order code stored")
-    }
-    console.log('Order complete!');
+        .then(function buyResponse(response){
+            console.log("Eventbrite order code submit response", res);
+            if (response.status === 200) { //&& res.headers.get('authorization')) {
+                console.log("Eventbrite order code stored");
+                console.log('Order complete!');
+                window.location.replace("/ticket-thank-you/");
+            }
+        })
 };
---
+++ src/ticket-thank-you.md
@@ -0,0 +1,13 @@
+---
+layout: layouts/page.njk
+title: Thank You!
+description: ''
+tags: pages
+ogImage: ''
+
+---
+
+Thank you for buying your ticket! We look forward to seeing you at the event.
+
+Looking to get involved?  Try these links!
+
