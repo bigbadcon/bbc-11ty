9677aea397455f870b78886624b457a15a9168e8
Colin Fahrion <colin.fahrion@gmail.com>
2022-09-23 13:10:56 -0700

    build: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+    },
+    "rules": {
+        'no-console': 'warn',
+    }
+}
