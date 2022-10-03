b800330ba1ea8114315450ac0ebf7445f3a33788
Colin Fahrion <colin.fahrion@gmail.com>
2022-10-02 17:12:30 -0700

    refactor: add eslint comments

--- functions/change-password.js
+++ functions/change-password.js
@@ -1,44 +1,53 @@
-const axios = require('axios');
+/* global require exports process */
+const axios = require("axios");
 
 // TODO: change to Prod API
 // const apiBaseUrl = 'http://www.logictwine.com:8092/'
-const apiBaseUrl = 'https://admin.bigbadcon.com:8091/api/'
+const apiBaseUrl = "https://admin.bigbadcon.com:8091/api/";
 
-const apiKey = `ApiKey ${process.env.BBC_API_KEY}`
+const apiKey = `ApiKey ${process.env.BBC_API_KEY}`;
 // headers: {"x-api-key": apiKey}
 
-exports.handler = async function(event, context) {
-    const {uuid,email,password} = event.queryStringParameters
-    console.log(uuid,email,password);
+exports.handler = async function (event) {
+	const { uuid, email, password } = event.queryStringParameters;
+	// eslint-disable-next-line no-console
+	console.log(uuid, email, password);
 
-    const body = {
-        "emailAddress": email,
-        "password": password,
-        "uuid": uuid
-    }
-    const headers = { headers: {"x-api-key": apiKey} }
+	const body = {
+		emailAddress: email,
+		password: password,
+		uuid: uuid,
+	};
+	const headers = { headers: { "x-api-key": apiKey } };
 
-    try {
+	try {
+		const res = await axios.post(
+			apiBaseUrl + `password/reset`,
+			body,
+			headers
+		);
 
-        const res = await axios.post(apiBaseUrl + `password/reset`, body, headers)
-
-        if (res.status === 200) {
-            return {
-                statusCode: 200,
-                body: "password changed",
-            }
-        } else {
-            return {
-                statusCode: 500,
-                body: "failed"
-            }
-        }
-    } catch (err) {
-        console.log("password change failed", err.response.config.url, err.response.status);
-        return {
-            statusCode: 200,
-            body: "failed"
-        }
-    }
-
-}
+		if (res.status === 200) {
+			return {
+				statusCode: 200,
+				body: "password changed",
+			};
+		} else {
+			return {
+				statusCode: 500,
+				body: "failed",
+			};
+		}
+	} catch (err) {
+		// eslint-disable-next-line no-console
+		console.log(
+			"password change failed",
+			err.response.config.url,
+			err.response.status
+		);
+		return {
+			statusCode: 200,
+			body: "failed",
+		};
+	}
+};
