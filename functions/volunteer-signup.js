c86bfc7f256e25a791ad0c9480201fab647c864c
Colin Fahrion <colin.fahrion@gmail.com>
2022-10-04 16:38:48 -0700

    fix: for remove role

--- functions/volunteer-signup.js
+++ functions/volunteer-signup.js
@@ -1,120 +1,149 @@
-const axios = require('axios');
-require('dotenv').config();
-const { GoogleSpreadsheet } = require('google-spreadsheet')
+/* global require process exports */
+
+const axios = require("axios");
+require("dotenv").config();
+const { GoogleSpreadsheet } = require("google-spreadsheet");
 
 // const apiBaseUrl = 'http://www.logictwine.com:8092/'
-const apiBaseUrl = 'https://admin.bigbadcon.com:8091/api/'
+const apiBaseUrl = "https://admin.bigbadcon.com:8091/api/";
 
-const apiKey = `ApiKey ${process.env.BBC_API_KEY}`
+const apiKey = `ApiKey ${process.env.BBC_API_KEY}`;
 // headers: {"x-api-key": apiKey}
 
-exports.handler = async function(event, context) {
+exports.handler = async function (event) {
+	if (event.httpMethod === "POST") {
+		/* -------------------------------------------------------------------------- */
+		/*                        1. grab submitted event data                        */
+		/* -------------------------------------------------------------------------- */
 
-    if (event.httpMethod === 'POST') {  
-        /* -------------------------------------------------------------------------- */
-        /*                        1. grab submitted event data                        */
-        /* -------------------------------------------------------------------------- */
+		const eventBody = JSON.parse(event.body);
 
-        const eventBody = JSON.parse(event.body)
+		/* -------------------------------------------------------------------------- */
+		/*                        2. Submit data to google form                       */
+		/* -------------------------------------------------------------------------- */
+		// eslint-disable-next-line no-console
+		console.log("Try submit to Google Sheet");
 
-        
+		try {
+			// Initialize the sheet - doc ID is the long id in the sheets URL
+			const doc = new GoogleSpreadsheet(
+				process.env.GOOGLE_SHEET_VOLUNTEER_BIGBADCON_2022
+			);
 
-        /* -------------------------------------------------------------------------- */
-        /*                        2. Submit data to google form                       */
-        /* -------------------------------------------------------------------------- */
+			// Initialize Auth - see more available options at https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
+			await doc.useServiceAccountAuth({
+				client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+				private_key: process.env.GOOGLE_PRIVATE_KEY.replace(
+					/\\n/g,
+					"\n"
+				),
+			});
 
-        console.log('Try submit to Google Sheet');
-        try {
-            // Initialize the sheet - doc ID is the long id in the sheets URL
-            const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_VOLUNTEER_BIGBADCON_2022)
+			await doc.loadInfo(); // loads document properties and worksheets
+			// eslint-disable-next-line no-console
+			console.log(doc.title);
+			const sheet = doc.sheetsByIndex[0];
 
-            // Initialize Auth - see more available options at https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
-            await doc.useServiceAccountAuth({
-                client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-                private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-            });
+			/* ---------------------- Take submit event and add row --------------------- */
 
-            await doc.loadInfo() // loads document properties and worksheets
-            console.log(doc.title);
-            const sheet = doc.sheetsByIndex[0]
+			const dateAdded = new Date().toLocaleDateString();
+			const addedRow = await sheet.addRow({
+				dateAdded: dateAdded,
+				displayName: eventBody.displayName,
+				userId: eventBody.userId,
+				userEmail: eventBody.userEmail,
+				userAge: eventBody.yourAge, //required
+				phone: eventBody.phone, //required
+				discord: eventBody.discord, //required
+				otherInfo: eventBody.otherInfo, //not required
+				communityStandards: eventBody.communityStandards && "Agreed",
+			});
+			// eslint-disable-next-line no-console
+			console.log("addedRow", addedRow);
 
-            /* ---------------------- Take submit event and add row --------------------- */
+			/* -------------------------------------------------------------------------- */
+			/*                        3. add volunteer role to use                        */
+			/* -------------------------------------------------------------------------- */
 
-            const dateAdded = new Date().toLocaleDateString()
-            const addedRow = await sheet.addRow({
-                dateAdded: dateAdded,
-                displayName: eventBody.displayName,
-                userId: eventBody.userId,
-                userEmail: eventBody.userEmail,
-                userAge: eventBody.yourAge, //required
-                phone: eventBody.phone, //required
-                discord: eventBody.discord, //required
-                otherInfo: eventBody.otherInfo, //not required
-                communityStandards: eventBody.communityStandards && "Agreed"
-            })
-            console.log("addedRow", addedRow);
+			const headers = { headers: { "x-api-key": apiKey } };
+			const userId = parseInt(eventBody.userId);
 
-            /* -------------------------------------------------------------------------- */
-            /*                        3. add volunteer role to use                        */
-            /* -------------------------------------------------------------------------- */
-        
-            
-            const headers = { headers: {"x-api-key": apiKey} }
-            const userId = parseInt(eventBody.userId)
+			try {
+				const res = await axios.post(
+					apiBaseUrl + `users/addRoleToUser`,
+					{
+						role: "volunteer",
+						userId: userId,
+					},
+					headers
+				);
 
-            try {
-                const res = await axios.post(apiBaseUrl + `users/addRoleToUser`, {
-                    "role": "volunteer",
-                    "userId": userId
-                }, headers)
-            
-                if (res.status !== 200) throw new Error("Add user role failed")
-            } catch (err) {
-                console.log("add user role for volunteer failed", err.toString());
-            }
+				if (res.status !== 200) throw new Error("Add user role failed");
+			} catch (err) {
+				// eslint-disable-next-line no-console
+				console.log(
+					"add user role for volunteer failed",
+					err.toString()
+				);
+			}
 
-            /* -------------------------------------------------------------------------- */
-            /*                    4. Remove notattending and subscriber role for user                    */
-            /* -------------------------------------------------------------------------- */
-            try {
-                const res = await axios.post(apiBaseUrl + `remove`, {
-                    "role": "notattending",
-                    "userId": userId
-                }, headers)
-            
-                if (res.status !== 200) throw new Error("Add user role failed")
-            } catch (err) {
-                console.log("remove user role 'notattending' for volunteer failed", err.toString());
-            }
+			/* -------------------------------------------------------------------------- */
+			/*                    4. Remove notattending and subscriber role for user                    */
+			/* -------------------------------------------------------------------------- */
+			try {
+				const res = await axios.post(
+					apiBaseUrl + `users/removeRoleFromUser`,
+					{
+						role: "notattending",
+						userId: userId,
+					},
+					headers
+				);
 
-            try {
-                const res = await axios.post(apiBaseUrl + `remove`, {
-                    "role": "subscriber",
-                    "userId": userId
-                }, headers)
-            
-                if (res.status !== 200) throw new Error("Add user role failed")
-            } catch (err) {
-                console.log("remove user role 'subscriber' for volunteer failed", err.toString());
-            }
+				if (res.status !== 200) throw new Error("Add user role failed");
+			} catch (err) {
+				// eslint-disable-next-line no-console
+				console.log(
+					"remove user role 'notattending' for volunteer failed",
+					err.toString()
+				);
+			}
 
-            return {
-                statusCode: 200,
-                body: "user added volunteer role",
-            }
+			try {
+				const res = await axios.post(
+					apiBaseUrl + `users/removeRoleFromUser`,
+					{
+						role: "subscriber",
+						userId: userId,
+					},
+					headers
+				);
 
-        } catch (e) {
-            console.log('Google Sheet add failed', e)
-            return {
-                statusCode: 500,
-                body: e.toString(),
-            }
-        }
+				if (res.status !== 200) throw new Error("Add user role failed");
+			} catch (err) {
+				// eslint-disable-next-line no-console
+				console.log(
+					"remove user role 'subscriber' for volunteer failed",
+					err.toString()
+				);
+			}
 
-    } else {
-        return {
-            statusCode: 500,
-            body: `only POST events are allowed`
-        }
-    }
-}
+			return {
+				statusCode: 200,
+				body: "user added volunteer role",
+			};
+		} catch (e) {
+			// eslint-disable-next-line no-console
+			console.log("Google Sheet add failed", e);
+			return {
+				statusCode: 500,
+				body: e.toString(),
+			};
+		}
+	} else {
+		return {
+			statusCode: 500,
+			body: "only post events are all allowed",
+		};
+	}
+};
