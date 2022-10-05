49e83e26dfb2662769d74dec258d6f160247a26b
Colin Fahrion <colin.fahrion@gmail.com>
2022-10-05 14:29:34 -0700

    feat: remove tags for now

--- functions/createCheckoutSession.js
+++ functions/createCheckoutSession.js
@@ -1,91 +1,96 @@
-require('dotenv').config();
-const environment = process.env.CONTEXT
-const apiKey = environment !== "production"
-    ? process.env.STRIPE_TEST_KEY
-    : process.env.STRIPE_SECRET_KEY
+/* global require process exports */
+require("dotenv").config();
+const environment = process.env.CONTEXT;
+const apiKey =
+	environment !== "production"
+		? process.env.STRIPE_TEST_KEY
+		: process.env.STRIPE_SECRET_KEY;
 
-const stripe = require('stripe')(apiKey)
+const stripe = require("stripe")(apiKey);
 
 /* -------------------------------------------------------------------------- */
 /*                              Helper Functions                              */
 /* -------------------------------------------------------------------------- */
 
 function paramsToObject(entries) {
-    const result = {}
-    for(const [key, value] of entries) { // each 'entry' is a [key, value] tupple
-      result[key] = value;
-    }
-    return result;
+	const result = {};
+	for (const [key, value] of entries) {
+		// each 'entry' is a [key, value] tupple
+		result[key] = value;
+	}
+	return result;
 }
 
 /* -------------------------------------------------------------------------- */
 /*                   Main Function for purchasing via Stripe                  */
 /* -------------------------------------------------------------------------- */
 
-exports.handler = async function (event, context) {
-    const referer = event.headers.referer
+exports.handler = async function (event) {
+	const referer = event.headers.referer;
 
-    // const body = event.body
-    // console.log("🚀 ~ file: createCheckoutSession.js ~ line 28 ~ event.body", event.body)
+	// const body = event.body
+	// console.log("🚀 ~ file: createCheckoutSession.js ~ line 28 ~ event.body", event.body)
 
-    const urlParams = new URLSearchParams(event.body)
-    const entries = urlParams.entries()
-    const params = paramsToObject(entries)
-    console.log("🚀 ~ file: createCheckoutSession.js ~ line 30 ~ params", params)
+	const urlParams = new URLSearchParams(event.body);
+	const entries = urlParams.entries();
+	const params = paramsToObject(entries);
+	// eslint-disable-next-line no-console
+	console.log(
+		"🚀 ~ file: createCheckoutSession.js ~ line 30 ~ params",
+		params
+	);
 
-    // TODO: make this more automated
-    let successUrl = "https://www.bigbadcon.com/thanks-for-your-purchase/"
-    if (params.productType === "badge") successUrl = "https://www.bigbadcon.com/buy-a-badge-thanks/"
-    if (params.productType === "poc-dinner") successUrl = "https://www.bigbadcon.com/poc-dinner-contribution-thanks/"
+	// TODO: make this more automated
+	let successUrl = "https://www.bigbadcon.com/thanks-for-your-purchase/";
+	if (params.productType === "badge")
+		successUrl = "https://www.bigbadcon.com/buy-a-badge-thanks/";
+	if (params.productType === "poc-dinner")
+		successUrl =
+			"https://www.bigbadcon.com/poc-dinner-contribution-thanks/";
 
-    try {
-        const session = await stripe.checkout.sessions.create({
-            line_items: [
-                {
-                    price: params.price_id,
-                    quantity: 1,
-                }
-            ],
-            customer_email: params.userEmail,
-            client_reference_id: params.userId,
-            metadata: {
-                userDisplayName: params.userDisplayName,
-                age: params.age,
-                recipient: params.recipient,
-                anon: params.anon,
-                recipientEmail: params.recipientEmail && params.recipientEmail.trim(),
-                productType: params.productType // "badge" or "poc dinner"
-            },
-            mode: "payment",
-            // TODO: customize thanks page with order details
-            success_url: successUrl,
-            cancel_url: referer,
-            // TODO: add metadata with other person's email address
-        })
+	try {
+		const session = await stripe.checkout.sessions.create({
+			line_items: [
+				{
+					price: params.price_id,
+					quantity: 1,
+				},
+			],
+			customer_email: params.userEmail,
+			client_reference_id: params.userId,
+			metadata: {
+				userDisplayName: params.userDisplayName,
+				age: params.age,
+				recipient: params.recipient,
+				anon: params.anon,
+				recipientEmail:
+					params.recipientEmail && params.recipientEmail.trim(),
+				productType: params.productType, // "badge" or "poc dinner"
+			},
+			mode: "payment",
+			// TODO: customize thanks page with order details
+			success_url: successUrl,
+			cancel_url: referer,
+			// TODO: add metadata with other person's email address
+		});
 
-        // console.log(session)
+		// console.log(session)
 
-        // return session url success or cancel_url
-        return {
-            statusCode: 303,
-            headers: {
-                Location: session.url,
-            }
-        }
-    } catch (e) {
-        console.log("🚀 ~ file: createCheckoutSession.js ~ line 78 ~ e", e)
-        return {
-            statusCode: 303,
-            headers: {
-                Location: "https://www.bigbadcon.com/buy-a-badge-fail/",
-            }
-        }
-    }
-
-    return {
-        statusCode: 303,
-        headers: {
-            Location: referer,
-        }
-    }
-}
+		// return session url success or cancel_url
+		return {
+			statusCode: 303,
+			headers: {
+				Location: session.url,
+			},
+		};
+	} catch (e) {
+		// eslint-disable-next-line no-console
+		console.log("🚀 ~ file: createCheckoutSession.js ~ line 78 ~ e", e);
+		return {
+			statusCode: 303,
+			headers: {
+				Location: "https://www.bigbadcon.com/buy-a-badge-fail/",
+			},
+		};
+	}
+};
