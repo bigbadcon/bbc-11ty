db8cd9ed782a44dacb9b9f8ef7c3e092ff87e67f
Colin Fahrion <colin.fahrion@gmail.com>
2022-11-04 11:27:42 -0700

    content: update event date

--- src/_data/sitemeta.js
+++ src/_data/sitemeta.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-    domain: process.env.DOMAIN || "www.bigbadcon.com",
-    url: process.env.URL || "https://www.bigbadcon.com",
-    environment: process.env.ELEVENTY_ENV,
-    context: process.env.CONTEXT,
-    eventDate: "Oct 27-30, 2022",
-    eventSignupDate: "Sun Sep 25 2022 11:50:00 GMT-0700 (PDT)"
+	domain: process.env.DOMAIN || "www.bigbadcon.com",
+	url: process.env.URL || "https://www.bigbadcon.com",
+	environment: process.env.ELEVENTY_ENV,
+	context: process.env.CONTEXT,
+	eventDate: "Sep 28 - Oct 1, 2023",
+	eventSignupDate: "Sun Sep 25 2022 11:50:00 GMT-0700 (PDT)",
 };
