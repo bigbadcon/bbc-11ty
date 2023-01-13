3b954b7646e8cfd6133db793a3e5bc2e626c60f6
Colin Fahrion <colin.fahrion@gmail.com>
2023-01-13 14:51:16 -0800

    test: remove plugin

--- functions/checkEnvironment.js
+++ functions/checkEnvironment.js
@@ -1,9 +1,10 @@
-require('dotenv').config();
-exports.handler = async function(event, context) {
-    const buildContext = process.env.CONTEXT
-    const NETLIFY = process.env.NETLIFY
-    return {
-        statusCode: 200,
-        body: `CONTEXT: ${buildContext} NETLIFY: ${NETLIFY}`
-    }
-}
+require("dotenv").config();
+exports.handler = async function (event, context) {
+	const buildContext = process.env.CONTEXT;
+	const NETLIFY = process.env.NETLIFY;
+	const URL = process.env.URL;
+	return {
+		statusCode: 200,
+		body: `CONTEXT: ${buildContext} NETLIFY: ${NETLIFY} URL: ${URL}`,
+	};
+};
