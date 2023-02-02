263dee4afc1afa12d59a6181a62ba9e68c5967d1
Colin Fahrion <colin.fahrion@gmail.com>
2023-02-01 21:07:34 -0800

    feat: add online date to header

--- src/_data/sitemeta.js
+++ src/_data/sitemeta.js
@@ -5,6 +5,6 @@ module.exports = {
 	url: process.env.URL || "https://www.bigbadcon.com",
 	environment: process.env.ELEVENTY_ENV,
 	context: process.env.CONTEXT,
+	eventDateOnline: "Mar 31 - Apr 1, 2023",
 	eventDate: "Sep 28 - Oct 1, 2023",
-	eventSignupDate: "Sun Sep 25 2022 11:50:00 GMT-0700 (PDT)",
 };
