371acb0c813f6bf51ea9618b49a44740996bf14d
Colin Fahrion <colin.fahrion@gmail.com>
2023-02-06 10:15:01 -0800

    feat: change to sitemeta to separate next event from online/irl

--- src/_data/sitemeta.js
+++ src/_data/sitemeta.js
@@ -6,5 +6,6 @@ module.exports = {
 	environment: process.env.ELEVENTY_ENV,
 	context: process.env.CONTEXT,
 	eventDateOnline: "Mar 31 - Apr 1 2023",
-	eventDate: "Sep 28 - Oct 1 2023",
+	eventDateIrl: "Sep 28 - Oct 1 2023",
+	eventDate: "Mar 31 - Apr 1 2023",
 };
