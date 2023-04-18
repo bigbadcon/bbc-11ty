14796af5c9d35c7420fb5fd090f02a144063cc2d
Colin Fahrion <colin.fahrion@gmail.com>
2023-04-18 14:37:13 -0600

    feat: remove login if data service not online

--- src/_data/sitemeta.js
+++ src/_data/sitemeta.js
@@ -1,11 +1,39 @@
 require("dotenv").config();
 
-module.exports = {
-	domain: process.env.DOMAIN || "www.bigbadcon.com",
-	url: process.env.URL || "https://www.bigbadcon.com",
-	environment: process.env.ELEVENTY_ENV,
-	context: process.env.CONTEXT,
-	eventDateOnline: false,
-	eventDateIrl: "Sep 28 - Oct 1 2023",
-	eventDate: "Sep 28 - Oct 1 2023",
+/* --------------------------- Check server status -------------------------- */
+function checkServer(url, timeout = 500) {
+	const controller = new AbortController();
+	const signal = controller.signal;
+	const options = { mode: "no-cors", signal };
+	fetch(url, options)
+		.then(
+			setTimeout(() => {
+				controller.abort();
+			}, timeout)
+		)
+		.then((response) => {
+			// eslint-disable-next-line no-console
+			console.log("Check server response:", response.statusText);
+			return response.statusText;
+		})
+		.catch((error) => {
+			// eslint-disable-next-line no-console
+			console.error("Check server error:", error.message);
+			return false;
+		});
+}
+
+module.exports = async () => {
+	const isAdminOnline = checkServer("https://admin.bigbadcon.com");
+
+	return {
+		domain: process.env.DOMAIN || "www.bigbadcon.com",
+		url: process.env.URL || "https://www.bigbadcon.com",
+		environment: process.env.ELEVENTY_ENV,
+		context: process.env.CONTEXT,
+		eventDateOnline: false,
+		eventDateIrl: "Sep 28 - Oct 1 2023",
+		eventDate: "Sep 28 - Oct 1 2023",
+		dataService: isAdminOnline,
+	};
 };
