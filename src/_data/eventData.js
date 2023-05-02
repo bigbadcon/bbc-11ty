b9c809c3e6dee8909bd148a90e4c2d52090fe258
Jeremy Tidwell <necanthrope@users.noreply.github.com>
2023-05-01 17:49:29 -0700

    Booking and Event Fixes

--- src/_data/eventData.js
+++ src/_data/eventData.js
@@ -26,6 +26,7 @@ const url = apiBaseUrl + "events/all/public"
 /* -------- Convert metadata array to object to make it easier to use ------- */
 function metadataArrayToObject(arr) {
     const object = arr.reduce(function(result, item) {
+        console.log("meta: " + item.metaKey + ", " + item.metaValue)
       result[item.metaKey] = item.metaValue;
       return result;
     }, {});
