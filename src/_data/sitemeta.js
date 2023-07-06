978adefc73c034d9e113e580ee2ea05ee318d005
Kona Goodhart <code@konahart.com>
2023-07-05 20:56:01 -0700

    Remove in-person event date

--- src/_data/sitemeta.js
+++ src/_data/sitemeta.js
@@ -3,9 +3,8 @@ require('dotenv').config();
 module.exports = {
     domain: process.env.DOMAIN || "www.goplaynw.org",
     url: process.env.URL || "https://www.goplaynw.org",
-    eventDate: "LIVE: 7 JUL - 9 JUL 2023 • Centilia Cultural Center ",
-    eventOnline: "ONLINE: 27 OCT - 29 OCT 2023 • Discord",
-    eventType: "IN-PERSON", // IN-PERSON, ONLINE, HYBRID
+    eventDate: "27 OCT - 29 OCT 2023 • Discord",
+    eventType: "ONLINE", // IN-PERSON, ONLINE, HYBRID
     signupsOpen: false,
     gameSubmissionOpen: false,
 };
