b7bc98ba26dbe17b882fe7ab76240ec2e4eca7c6
Kona Goodhart <code@konahart.com>
2023-07-05 21:59:59 -0700

    Set up off-season navigation

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,30 +1,31 @@
 module.exports = [
-	// {
-	// 	"title": "Go Play NW",
-	// 	"id": "off-season",
-	// 	"subnav": [
-	// 		{
-	// 		 	"title": "What is Go Play NW?",
-	// 		 	"url": "/what-is-go-play-nw",
-	// 		 	"icon": "goplaynw"
-	// 		},
-	// 		{
-	// 			"title": "Things to Do",
-	// 			"url": "/register",
-	// 			"icon": "badge"
-	// 	   },
-	// 	   {
-	// 			"title": "Featured Facilitators",
-	// 			"url": "/featured-facilitators",
-	// 			"icon": "trophy"
-	// 		},
-	// 		{
-	// 			"title": "Get Involved",
-	// 			 "url": "/get-involved",
-	// 			 "icon": "donut"
-	// 		}
-	// 	]
-	// },
+	{
+		"title": "Go Play NW",
+		"id": "off-season",
+		"subnav": [
+			{
+				"title": "What is Go Play NW?",
+				"url": "/about-us",
+				"icon": "goplaynw"
+			},
+			{
+				"title": "The Donut",
+				"url": "/the-donut",
+				"icon": "donut"
+			},
+			{
+				"title": "Featured Facilitators",
+				"url": "/featured-facilitators",
+				"icon": "trophy"
+			},
+			{
+				"title": "Get Involved",
+				"url": "/volunteer",
+				"icon": "people-group"
+			},
+		]
+	},
+	/*
 	{
 		"title": "Attend",
 		"id": "attend",
@@ -58,7 +59,7 @@ module.exports = [
 				"title" : "COVID-19 Policy",
 				"url" : "/covid-19",
 				"icon" : "virus"
-			},			
+			},
 		]
 	},
 	{
@@ -87,37 +88,37 @@ module.exports = [
 			}
 		]
 	},
-	// {
-	// 	"title": "Play",
-	// 	"id": "play-online",
-	// 	"subnav": [
-	// 		{
-	// 			"title": "Run a Game",
-	// 			"url": "/run-an-event",
-	// 			"icon": "dice"
-	// 		},
-	// 		{
-	// 			"title": "Scheduled Games",
-	// 		 	"url": "/events",
-	// 		 	"icon": "calendar-clock"
-	// 		},
-	// 		{
-	// 			"title": "The Donut",
-	// 		 	"url": "/the-donut-online",
-	// 		 	"icon": "donut"
-	// 		},
-	// 		{
-	// 			"title": "Featured Facilitators",
-	// 			"url": "/featured-facilitators",
-	// 			"icon": "trophy"
-	// 		},
-	// 		{
-	// 			"title": "Online Game Inspiration",
-	// 			"url": "online-games-list/",
-	// 			"icon": ""
-	// 		}
-	// 	]
-	// },
+		 {
+		 	"title": "Play",
+		 	"id": "play-online",
+		 	"subnav": [
+		 		{
+		 			"title": "Run a Game",
+		 			"url": "/run-an-event",
+		 			"icon": "dice"
+		 		},
+		 		{
+		 			"title": "Scheduled Games",
+		 		 	"url": "/events",
+		 		 	"icon": "calendar-clock"
+		 		},
+		 		{
+		 			"title": "The Donut",
+		 		 	"url": "/the-donut-online",
+		 		 	"icon": "donut"
+		 		},
+		 		{
+		 			"title": "Featured Facilitators",
+		 			"url": "/featured-facilitators",
+		 			"icon": "trophy"
+		 		},
+		 		{
+		 			"title": "Online Game Inspiration",
+		 			"url": "online-games-list/",
+		 			"icon": ""
+		 		}
+		 	]
+		 },
 	{
 		"title": "Volunteer",
 		"id": "volunteer",
@@ -126,13 +127,13 @@ module.exports = [
 				"url": "/volunteer",
 				"icon": "people-group"
 			},
-			{	
+			{
 				"title": "Volunteer Shifts",
 				"url": "/volunteer-shifts/",
 				"icon": "people-group"
 			}
 		]
-	},
+	},*/
 	{
 		"title": "About Us",
 		"id": "about",
