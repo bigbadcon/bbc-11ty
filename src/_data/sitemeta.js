4d0579518abb59472e8152d3427bbc334cab2d27
Kona Goodhart <code@konahart.com>
2023-07-05 22:24:26 -0700

    Separate event date and location in sitemeta

--- src/_data/sitemeta.js
+++ src/_data/sitemeta.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 module.exports = {
     domain: process.env.DOMAIN || "www.goplaynw.org",
     url: process.env.URL || "https://www.goplaynw.org",
-    eventDate: "27 OCT - 29 OCT 2023 • Discord",
+    eventDate: "27 OCT - 29 OCT 2023",
+    eventLocation: "Discord",
     eventType: "ONLINE", // IN-PERSON, ONLINE, HYBRID
     signupsOpen: false,
     gameSubmissionOpen: false,
