553721677ef9d61ad94c4970982a27cc3babf946
Colin Fahrion <colin.fahrion@gmail.com>
2023-10-10 12:34:56 -0700

    fix: upgrade esbuild

--- esbuild.js
+++ esbuild.js
@@ -27,10 +27,19 @@ function build(options) {
 	options.define["process.env.NODE_ENV"] = process.argv.includes("--watch") ? `'production'` : `'development'`;
 
 	return require("esbuild")
-		.build({
-			watch: process.argv.includes("--watch"),
-			// external: ['alpinejs'],
+		.context({
 			...options,
+		})
+		.then((context) => {
+			if (process.argv.includes("--watch")) {
+				// Enable watch mode
+				context.watch();
+			} else {
+				// Build once and exit if not in watch mode
+				context.rebuild().then((result) => {
+					context.dispose();
+				});
+			}
 		})
 		.catch(() => process.exit(1));
 }
