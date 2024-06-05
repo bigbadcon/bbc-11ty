bf8614aada75cde5f3cfd0d3e3befb15eeddcf80
Colin Fahrion <colin.fahrion@gmail.com>
2024-06-05 14:49:21 -0700

    feat: update badge claim script to work with latest google sheets

--- netlify/functions/check-claim-badge.js
+++ netlify/functions/check-claim-badge.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const axios = require("axios");
 const { GoogleSpreadsheet } = require("google-spreadsheet");
+const { JWT } = require("google-auth-library");
 
 // BBC API
 const bbcApiBaseUrl = "https://admin.bigbadcon.com:8091/api/";
@@ -33,8 +34,17 @@ exports.handler = async function (event) {
 	// console.log("🚀 ~ file: check-claim-badge.js ~ params", params);
 
 	try {
+		const GOOGLE_PRIVATE_KEY = process.env.GOOGLE_PRIVATE_KEY;
+		const GOOGLE_SERVICE_ACCOUNT_EMAIL = process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL;
+
 		// Initialize the sheet - doc ID is the long id in the sheets URL
-		const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_BADGE_CLAIM);
+		const serviceAccountAuth = new JWT({
+			email: GOOGLE_SERVICE_ACCOUNT_EMAIL,
+			key: GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+			scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+		});
+		// Initialize the sheet - doc ID is the long id in the sheets URL
+		const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_BADGE_CLAIM, serviceAccountAuth);
 
 		// Initialize Auth - see more available options at https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
 		await doc.useServiceAccountAuth({
@@ -45,8 +55,8 @@ exports.handler = async function (event) {
 		await doc.loadInfo(); // loads document properties and worksheets
 
 		// Get main sheet and rows
-		// TODO: update so it automatically uses the current year
-		const sheet = await doc.sheetsByIndex[0];
+		const year = new Date().getFullYear().toString();
+		let sheet = doc.sheetsByTitle[year];
 		const rows = await sheet.getRows();
 
 		// Search for Badge Claim Code to see if it exists
