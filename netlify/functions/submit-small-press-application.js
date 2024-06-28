00be84ab06251e3d3b8e2bdcbecdff642d057d2b
Colin Fahrion <colin.fahrion@gmail.com>
2024-06-28 12:22:37 -0700

    fix: remove gmCode from vending; update Small Press Form

--- netlify/functions/submit-small-press-application.js
+++ netlify/functions/submit-small-press-application.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 require("dotenv").config();
 const { GoogleSpreadsheet } = require("google-spreadsheet");
+const { JWT } = require("google-auth-library");
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const environment = process.env.CONTEXT;
@@ -26,22 +27,24 @@ exports.handler = async function (event, context) {
 		console.log("Try submit to Google Sheet");
 		try {
 			// Initialize the sheet - doc ID is the long id in the sheets URL
-			const doc = new GoogleSpreadsheet(googleSheetId);
-
-			// Initialize Auth - see more available options at https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
-			await doc.useServiceAccountAuth({
-				client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-				private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+			const serviceAccountAuth = new JWT({
+				email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+				key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+				scopes: ["https://www.googleapis.com/auth/spreadsheets"],
 			});
+
+			// Initialize the sheet - doc ID is the long id in the sheets URL
+			const doc = new GoogleSpreadsheet(googleSheetId, serviceAccountAuth);
 
 			await doc.loadInfo(); // loads document properties and worksheets
 			console.log(doc.title);
-			const sheet = doc.sheetsByIndex[0];
+			const year = new Date().getFullYear();
+			const sheet = doc.sheetsByTitle[year.toString()];
 
 			/* ---------------------- Take submit event and add row --------------------- */
 
 			const dateAdded = new Date().toLocaleDateString();
-			const addedRow = await sheet.addRow({ ...eventBody, dateAdded: dateAdded });
+			await sheet.addRow({ ...eventBody, dateAdded: dateAdded });
 			console.log(`added google sheet row for ${eventBody.userDisplayName}`);
 
 			/* -------------------------------------------------------------------------- */
