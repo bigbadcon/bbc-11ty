b3158102d1c56f6143b8e47b674b013e81530fc8
Colin Fahrion <colin.fahrion@gmail.com>
2024-08-07 21:16:55 -0700

    feat: login back to not reload

--- src/_scripts/scripts.js
+++ src/_scripts/scripts.js
@@ -174,7 +174,7 @@ document.addEventListener("alpine:init", () => {
 					await this.getUserData();
 					await this.getAvailableSlots();
 					await this.getBookedEvents();
-					this.modal = null;
+					this.modal = "My Account";
 					await this.getFavEvents();
 					// trigger storage event for Run an Event Form
 					window.dispatchEvent(
@@ -182,7 +182,7 @@ document.addEventListener("alpine:init", () => {
 							key: "_x_authToken",
 						})
 					);
-					location.reload();
+					// location.reload();
 				} else {
 					// eslint-disable-next-line no-console
 					console.log("Login no token");
