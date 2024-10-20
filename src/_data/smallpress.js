7dba06d56ab9c8808090a7d8de5979a5697e6a58
Colin Fahrion <colin.fahrion@gmail.com>
2024-10-20 13:52:51 -0700

    feat: made small press table wider; made it check to make sure the data exists

--- src/_data/smallpress.js
+++ src/_data/smallpress.js
@@ -4,28 +4,33 @@ const { JWT } = require("google-auth-library");
 
 // TODO: update this to grab current year
 module.exports = async () => {
-	const serviceAccountAuth = new JWT({
-		email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-		key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-		scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-	});
+	try {
+		const serviceAccountAuth = new JWT({
+			email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+			key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+			scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+		});
 
-	const doc = new GoogleSpreadsheet("1oc96lokG4l1Li0X8yoTQNNLejrpOUmheu1YaL2ISL1w", serviceAccountAuth);
+		const doc = new GoogleSpreadsheet("1oc96lokG4l1Li0X8yoTQNNLejrpOUmheu1YaL2ISL1w", serviceAccountAuth);
 
-	await doc.loadInfo();
+		await doc.loadInfo();
 
-	const sheet = doc.sheetsByIndex[0];
-	const rows = await sheet.getRows();
-	const json = rows.map((row) => {
-		return {
-			date: row.get("Date"),
-			start: row.get("Start"),
-			end: row.get("End"),
-			business: row.get("Business Name"),
-			website: row.get("Website"),
-			product: row.get("Product"),
-		};
-	});
+		const sheet = doc.sheetsByIndex[0];
+		const rows = await sheet.getRows();
+		const json = rows.map((row) => {
+			return {
+				date: row.get("Date"),
+				start: row.get("Start"),
+				end: row.get("End"),
+				business: row.get("Business Name"),
+				website: row.get("Website"),
+				product: row.get("Product"),
+			};
+		});
 
-	return json;
+		return json;
+	} catch (error) {
+		console.log(error);
+		return null;
+	}
 };
